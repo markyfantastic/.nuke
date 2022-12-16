d528a5dcdc2099f0285ebcff6af78fd57f59af5f
markyfantastic <96522298+markyfantastic@users.noreply.github.com>
2022-12-16 12:33:09 +0100

    Update menu.py
    Original menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,17 @@
+import nuke
+import platform
 
+# Define where .nuke directory is on each OS's network.
+Win_Dir = 'C:\Users\babis\.nuke'
+MacOSX_Dir = 'Users/babis/.nuke'
+Linux_Dir = 'home/babis/.nuke'
+
+# Set global directory
+if platform.system() == "Windows":
+	dir = Win_Dir
+elif platform.system() == "Darwin":
+	dir = MacOSX_Dir
+elif platform.system() == "Linux":
+	dir = Linux_Dir
+else:
+	dir = None
